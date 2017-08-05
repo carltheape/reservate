1c5ccd3d93f18c66e14d88b27a7197db96bdd274
mmann30 <michael.l.mann13@gmail.comm>
2017-08-05 11:56:09 -0400

    added routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
-// var path = require('path');  do we need this?
+var path = require('path');
 
 // Initializes the Express app
 var app = express();
@@ -10,9 +10,26 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Middleware goes here...
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.text());
+//app.use(bodyParser.json({ type: }))
 
+// Routes
 
-// Routes go here....
+// homepage 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/home.html"));
+});
+// reservation form
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/reserve.html"));
+});
+// view tables
+app.get("/table", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/table.html"));
+});
+
 
 
 // Starts the server
